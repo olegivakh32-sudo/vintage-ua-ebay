779f292ae9b99671303f9cee31be5c1c92cf5690
olegivakh32-sudo <olegivakh32@gmail.com>
2026-09-12 23:06:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ app.secret_key = os.environ.get(
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+EBAY_REFRESH_TOKEN = os.environ.get("EBAY_REFRESH_TOKEN")
 EBAY_CLIENT_ID = os.environ.get("EBAY_CLIENT_ID")
 EBAY_CLIENT_SECRET = os.environ.get("EBAY_CLIENT_SECRET")
 EBAY_RUNAME = os.environ.get("EBAY_RUNAME")
